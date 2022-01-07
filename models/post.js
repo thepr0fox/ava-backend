ba425aed91d6e148ce719157226e70b35618a84f
RndomONE <79525620+RndomONE@users.noreply.github.com>
2022-01-07 13:42:18 +0530

    Added functionality to post

---
+++ models/post.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const postSchema = new mongoose.Schema({
+  heading: {
+    type: String,
+    required: true,
+    default: 'No heading',
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "User",
+  },
+  postedAt: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Post", postSchema);
